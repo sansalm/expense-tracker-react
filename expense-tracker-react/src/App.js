6b4a0e12d70552764517920536cc4d23c44a763a
Liisukki <91130860+Liisukki@users.noreply.github.com>
2022-04-08 11:07:52 +0300

    Demo

--- expense-tracker-react/src/App.js
+++ expense-tracker-react/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+import React from 'react';
+import { Header } from './Components/Header';
+import { Balance } from './Components/Balance';
+import { IncomeExpenses } from './Components/IncomeExpenses';
+import { TransactionList } from './Components/TransactionList';
+import { AddTransaction } from './Components/AddTransaction';
+
+
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
+      <div className='container'></div>
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
     </div>
   );
 }
