fac119f6ab7840697e452c361c7302ff2381b57c
Liisukki <91130860+Liisukki@users.noreply.github.com>
2022-04-22 10:38:51 +0300

    Committed to wrong branch and trying to fix things

--- E03/E03T05.js
+++
@@ -1,9 +0,0 @@
-let person = {name: "Pentti", age: 22, country: "Finland"};
-
-let personString = JSON.stringify(person);
-
-let reCreatedPerson = JSON.parse(personString);
-
-console.log(person)
-
-console.log(personString)
--- E06/E06T04b.js
+++
@@ -1,9 +0,0 @@
-var input1 = document.getElementById("saveServer");
-
-function setitem() {
-  localStorage.setItem("server", input1.value);
-  console.log(localStorage.getItem("server"));
-  }
-
-console.log(localStorage.getItem("server"));
-input1.addEventListener("blur", setitem);
